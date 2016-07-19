26132805f0fa9d65614d05f3c2c0d36ba01dfd6d
kfiroo <kfirgolan@gmail.com>
2016-07-19 20:44:04 +0300

    add all services

--- src/SendBird.js
+++ src/SendBird.js
@@ -2,6 +2,10 @@
 
 var createService = require('./utils/createService');
 
+var adminService = require('./services/adminService');
+var channelService = require('./services/channelService');
+var messagingService = require('./services/messagingService');
+var migrationService = require('./services/migrationService');
 var userService = require('./services/userService');
 
 function SendBird(apiToken) {
@@ -11,6 +15,10 @@ function SendBird(apiToken) {
     if (!apiToken) {
         throw new Error('Invalid api token');
     }
+    this.adminService = createService(apiToken, adminService);
+    this.channelService = createService(apiToken, channelService);
+    this.messagingService = createService(apiToken, messagingService);
+    this.migrationService = createService(apiToken, migrationService);
     this.userService = createService(apiToken, userService);
 }
 
